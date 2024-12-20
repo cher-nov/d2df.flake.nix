1b79eb8422ab70287561ee1fde6bfe40060c115c
polybluez <163671824+polybluez@users.noreply.github.com>
2024-12-20 19:24:57 +0000

    Use .wad extension (even though it is a .zip)

--- game/assets/mkAssetsPath.nix
+++ game/assets/mkAssetsPath.nix
@@ -26,11 +26,11 @@ stdenvNoCC.mkDerivation {
     mkdir -p data/models wads maps/megawads/
     cp ${doom2dWad} maps/megawads/doom2d.dfz
     cp ${doomerWad} data/models/doomer.dfz
-    cp ${shrshadeWad} wads/shrshade
-    cp ${standartWad} wads/standart
-    cp ${editorWad} data/editor
+    cp ${shrshadeWad} wads/shrshade.wad
+    cp ${standartWad} wads/standart.wad
+    cp ${editorWad} data/editor.wad
     cp ${gameWad} data/game.dfz
-    cp ${flexuiWad} data/flexui
+    cp ${flexuiWad} data/flexui.wad
     cp ${botlist} data/botlist.txt
     cp ${botnames} data/botnames.txt
   '';
