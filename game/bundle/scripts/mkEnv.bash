89e756e4124b087b7802bab63f5b6a5c926e70ca
polybluez <163671824+polybluez@users.noreply.github.com>
2025-01-05 05:22:22 -0500

    Print original distro content archive name in release body

--- game/bundle/scripts/mkEnv.bash
+++ game/bundle/scripts/mkEnv.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-export TZ="Europe/Moscow"
+TZ="Europe/Moscow"
+export TZ=$TZ
 nix flake update d2df-sdl d2df-editor DF-res
 
 NIXPKGS_REV=$(nix flake metadata . --json 2>/dev/null | jq --raw-output '.locks.nodes."nixpkgs".locked.rev')
@@ -17,6 +18,7 @@ EDITOR_LAST_COMMIT_DATE=$(git --git-dir d2df-editor/.git show -s --format=%ad --
 RES_LAST_COMMIT_DATE=$(git    --git-dir DF-Res/.git      show -s --format=%ad --date=iso $RES_REV)
 DISTRO_CONTENT_CREATION_DATE="$(rar ltb '-x*' "$DISTRO_CONTENT_PATH" | grep "Original time" | cut -d' ' -f3-)"
 DISTRO_CONTENT_CREATION_DATE_PRETTY="$(date --date="$DISTRO_CONTENT_CREATION_DATE" +'%d %B %Y %H:%M %Z')"
+DISTRO_CONTENT_CREATION_NAME="$(rar ltb '-x*' "$DISTRO_CONTENT_PATH" | grep "Original name" | cut -d' ' -f3-)"
 
 echo "NIXPKGS_REV=$NIXPKGS_REV" >> "$GITHUB_ENV"
 echo "D2DF_REV=$D2DF_REV" >> "$GITHUB_ENV"
@@ -27,6 +29,7 @@ echo "EDITOR_LAST_COMMIT_DATE=$EDITOR_LAST_COMMIT_DATE" >> "$GITHUB_ENV"
 echo "RES_LAST_COMMIT_DATE=$RES_LAST_COMMIT_DATE" >> "$GITHUB_ENV"
 echo "DISTRO_CONTENT_CREATION_DATE=$DISTRO_CONTENT_CREATION_DATE" >> "$GITHUB_ENV"
 echo "DISTRO_CONTENT_CREATION_DATE_PRETTY=$DISTRO_CONTENT_CREATION_DATE_PRETTY" >> "$GITHUB_ENV"
+echo "DISTRO_CONTENT_CREATION_NAME=$DISTRO_CONTENT_CREATION_NAME" >> "$GITHUB_ENV"
 printf \
-    'This build has the following inputs:\nd2df-sdl: %s\nDF-res: %s\nd2df-editor: %s\nnixpkgs: %s\n\nGame content used for this build is as of: %s' \
-    "$D2DF_REV" "$RES_REV" "$EDITOR_REV" "$NIXPKGS_REV" "$DISTRO_CONTENT_CREATION_DATE_PRETTY" > release_body
+    'This build has the following inputs:\nd2df-sdl: %s\nDF-res: %s\nd2df-editor: %s\nnixpkgs: %s\n%s - %s' \
+    "$D2DF_REV" "$RES_REV" "$EDITOR_REV" "$NIXPKGS_REV" "$DISTRO_CONTENT_CREATION_NAME" "$DISTRO_CONTENT_CREATION_DATE_PRETTY" > release_body
