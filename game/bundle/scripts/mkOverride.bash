8768d0d90f23cd6ba2d92903f1462fa000c331a4
polybluez <163671824+polybluez@users.noreply.github.com>
2025-02-24 13:38:35 +0300

    CI: use old environment variables for dates

--- game/bundle/scripts/mkOverride.bash
+++ game/bundle/scripts/mkOverride.bash
@@ -4,8 +4,8 @@ CMD=$(
 cat <<EOF
 (builtins.getFlake (toString ./.)).outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
 (prev: {
-  executables = prev.executables.override {gameDate = "$GAME_DATE"; withDates = true;};
-  assets = prev.assets.override {editorDate = "$EDITOR_DATE"; assetsDate = "$ASSETS_DATE"; withDates = true;};
+  executables = prev.executables.override {gameDate = "$D2DF_LAST_COMMIT_DATE"; withDates = true;};
+  assets = prev.assets.override {editorDate = "$EDITOR_LAST_COMMIT_DATE"; assetsDate = "$RES_LAST_COMMIT_DATE"; withDates = true;};
 })
 EOF
 )
