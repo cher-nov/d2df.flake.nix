174feb5e614c4ea9f16e3995eba2d109e70e7cf0
polybluez <163671824+polybluez@users.noreply.github.com>
2025-02-24 12:40:04 +0300

    CI: always restore toolchain from cache

--- game/bundle/scripts/mkOverride.bash
+++ game/bundle/scripts/mkOverride.bash
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-EDITOR_DATE="2025-02-21 17:58:06 +1000"
-GAME_DATE="2025-02-21 17:58:06 +1000"
-ASSETS_DATE="2025-02-21 17:58:06 +1000"
 CMD=$(
 cat <<EOF
 (builtins.getFlake (toString ./.)).outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
