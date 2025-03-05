f5ff5f523e1f44574dd969ce841fef704732287e
polybluez <163671824+polybluez@users.noreply.github.com>
2025-03-05 13:43:27 +0300

    CI: remove impurity in mkOverride

--- game/bundle/scripts/mkOverride.bash
+++ game/bundle/scripts/mkOverride.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 CMD=$(
 cat <<EOF
-(builtins.getFlake (toString ./.)).outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
+(builtins.getFlake "git+file://$(pwd)").outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
 (prev: {
   executables = prev.executables.override {gameDate = "$D2DF_LAST_COMMIT_DATE"; editorDate = "$EDITOR_LAST_COMMIT_DATE"; withDates = true;};
   assets = prev.assets.override {editorDate = "$EDITOR_LAST_COMMIT_DATE"; assetsDate = "$RES_LAST_COMMIT_DATE"; withDates = true;};
