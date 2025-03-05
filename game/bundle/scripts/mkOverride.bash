a15d0cf29b43ca3ebbf12850454dc7ad7730f59b
polybluez <163671824+polybluez@users.noreply.github.com>
2025-03-05 15:04:43 +0300

    CI: remove shallow flag from mkOverride

--- game/bundle/scripts/mkOverride.bash
+++ game/bundle/scripts/mkOverride.bash
@@ -3,7 +3,7 @@ set -euo pipefail
 LAST_COMMIT=$(git rev-parse HEAD)
 CMD=$(
 cat <<EOF
-(builtins.getFlake "git+file://$(pwd)?rev=${LAST_COMMIT}&shallow=1").outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
+(builtins.getFlake "git+file://$(pwd)?rev=${LAST_COMMIT}").outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
 (prev: {
   executables = prev.executables.override {gameDate = "$D2DF_LAST_COMMIT_DATE"; editorDate = "$EDITOR_LAST_COMMIT_DATE"; withDates = true;};
   assets = prev.assets.override {editorDate = "$EDITOR_LAST_COMMIT_DATE"; assetsDate = "$RES_LAST_COMMIT_DATE"; withDates = true;};
