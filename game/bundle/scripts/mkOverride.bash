c46907ad462e6862af86c3a67b44424cc9afd1c8
polybluez <163671824+polybluez@users.noreply.github.com>
2025-03-05 19:12:47 +0300

    CI: change builds to be pure

--- game/bundle/scripts/mkOverride.bash
+++ game/bundle/scripts/mkOverride.bash
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -euo pipefail
-LAST_COMMIT=$(git rev-parse HEAD)
-CMD=$(
-cat <<EOF
-(builtins.getFlake (builtins.toString ./.)).outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
-(prev: {
-  executables = prev.executables.override {gameDate = "$D2DF_LAST_COMMIT_DATE"; editorDate = "$EDITOR_LAST_COMMIT_DATE"; withDates = true;};
-  assets = prev.assets.override {editorDate = "$EDITOR_LAST_COMMIT_DATE"; assetsDate = "$RES_LAST_COMMIT_DATE"; withDates = true;};
-})
-EOF
-)
-nix build \
-    --verbose --show-trace --verbose --print-out-paths --print-build-logs \
-    --impure --expr "$CMD"
+#D2DF_LAST_COMMIT_DATE="2025-03-05 17:42:45 +1000"
+#EDITOR_LAST_COMMIT_DATE="2025-03-05 00:05:50 +1000"
+#RES_LAST_COMMIT_DATE="2025-02-23 14:52:10 +1000"
+#LAST_COMMIT=$(git rev-parse HEAD)
+#CMD=$(
+#cat <<EOF
+#(builtins.getFlake (builtins.toString ./.)).outputs.legacyPackages.x86_64-linux.$BUNDLE_NAME.override
+#(prev: {
+#  executables = prev.executables.override {gameDate = "$D2DF_LAST_COMMIT_DATE"; editorDate = "$EDITOR_LAST_COMMIT_DATE"; withDates = true;};
+#  assets = prev.assets.override {editorDate = "$EDITOR_LAST_COMMIT_DATE"; assetsDate = "$RES_LAST_COMMIT_DATE"; withDates = true;};
+#})
+#EOF
+#   )
+
+# This breaks the game WADs build. Under investigation!
+#nix build \
+#    --verbose --show-trace --print-out-paths --print-build-logs \
+#    --impure --expr "$CMD"
+nix build --verbose --show-trace --print-out-paths --print-build-logs ".#$BUNDLE_NAME"
